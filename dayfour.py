7937d40959d91d965eb5ae68adbb11806833874f
Dell <Dell@VANESSA-JIDEONW>
2025-05-17 20:11:54 +0100

    Initial commit

---
+++ dayfour.py
@@ -0,0 +1,43 @@
+#ARITHMETIC Operations
+# addition = 3 + 5
+# subtraction = 7 - 2
+# # division with remainders
+# div1 = 4/2
+# # with only whole number
+# div2 = 3//2
+# # the remainder
+# div3 = 3%2
+# # raise to power
+# power = 10 ** 3
+# # incremented operator
+# x = 10
+# x = x + 3
+# # Augmented operator
+# x += 3
+
+#Project1
+# print('Simple Budget Calculator')
+# money = int(input('How much money do you have? '))
+# price1 = int(input('Enter price of item 1 '))
+# price2 = int(input('Enter price of item 2 '))
+# price3 = int(input('Enter price of item 3 '))
+# totalcost = price1 + price2 + price3
+# moneyleft = money -  totalcost
+# averageitem = float(totalcost / 3)
+# print(f'Item 1 price: {price1}')
+# print(f'Item 2 price: {price2}')
+# print(f'Item 3 price {price3}')
+# print(f'Total cost: {totalcost}')
+# print(f'Balance left: {moneyleft}')
+# print(f'Average item cost: {averageitem:.2f}')
+
+# Project2
+print('TO CALCULATE YOUR AGE IN MONTHS, WEEKS AND DAYS')
+age = int(input('Enter your age in years '))
+months = age * 12
+weeks = age * 52
+days = age * 365
+print('You Are')
+print(f'{months} months old')
+print(f'{weeks} weeks old')
+print(f'{days} days old')
---
+++ op.py
@@ -0,0 +1,14 @@
+#operator precidence
+#parenthesis
+# exponentiation 2 ** 2
+# multiplication or division
+# addition or subtraction
+
+import math
+#MATHS FUNCTION
+a = 2.9
+# print(round(a))
+# abs always return a postive number
+# print(abs(a))
+print(math.ceil(a))
+print(math.floor(a))
